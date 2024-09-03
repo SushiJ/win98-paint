765f91d6d54c68a11bf305e9f77bd6aa1a8f4e91
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-03 20:53:36 +0530

    Modal for exports

---
+++ src/components/Modal.tsx
@@ -0,0 +1,33 @@
+import { type ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+export function Modal({ children }: { children: ReactNode }) {
+  const modalRoot = document.getElementById("file-modal") as HTMLElement;
+  return createPortal(
+    <dialog
+      style={{
+        zIndex: "50",
+        position: "absolute",
+        height: "100vh",
+        width: "100vw",
+        display: "grid",
+        placeItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.4)",
+        backdropFilter: "blur(1px)",
+      }}
+    >
+      <div
+        className="window"
+        style={{
+          alignSelf: "center",
+          // width: "min-content",
+          // height: "min-content",
+          padding: "0.5rem",
+        }}
+      >
+        {children}
+      </div>
+    </dialog>,
+    modalRoot,
+  );
+}
