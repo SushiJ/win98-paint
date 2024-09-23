0f95a4b25f8c6454f4d3ee2a845db68acccd3f0e
Sushant Sharma <sushant.wurk@gmail.com>
2024-09-24 02:56:15 +0530

    ref. switch out to dpr instead of hard coded value of 2

--- src/utils/canvasUtils.ts
+++ src/utils/canvasUtils.ts
@@ -16,11 +16,14 @@ export const setCanvasSize = (
   width: number,
   height: number
 ) => {
-  canvas.width = width * 2;
-  canvas.height = height * 2;
+  const dpr = window.devicePixelRatio;
+  const rect = canvas.getBoundingClientRect();
+
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
   canvas.style.width = `${width}px`;
   canvas.style.height = `${height}px`;
-  canvas.getContext("2d")?.scale(2, 2);
+  canvas.getContext("2d")?.scale(dpr, dpr);
 };
 
 export const drawStroke = (
